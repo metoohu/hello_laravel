35d0101add4fda893d110228f203a7b67022b33e
edwin <metoohu@126.com>
2023-07-01 14:47:22 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.default')
 
 @section('content')
-  <h1>主页111</h1>
+<div class="bg-light p-3 p-sm-5 rounded">
+  <h1>欢迎来到我的地盘</h1>
+  <p class="lead">
+    你现在所看到的是 一片不一样的天地。
+  </p>
+  <p>
+    一切，将从这里开始。
+  </p>
+  <p>
+    <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+  </p>
+</div>
 @stop
