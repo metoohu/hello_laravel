a516a6bab22c208248d36f572753383979f38bd6
edwin <metoohu@126.com>
2023-07-04 14:36:15 +0800

    模板路由命名及替换

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,9 @@
 <footer class="footer">
-  <a href="#" target=_blank class="text-decoration-none">
+  <a href="{{route('home')}}" target=_blank class="text-decoration-none">
     刻意练习，每日精进
   </a>
 
   <div class="float-end">
-    <a href="/about" class="text-decoration-none">关于</a>
+    <a href="{{ route('help') }}" class="text-decoration-none">关于</a>
   </div>
 </footer>
