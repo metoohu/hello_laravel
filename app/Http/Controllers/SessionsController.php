41cbb92bc4ba26f32871927b28c43d8f77cee5cb
edwin <metoohu@126.com>
2023-07-10 20:30:26 +0800

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -29,4 +29,12 @@ class SessionsController extends Controller
             return redirect()->back()->withInput();
         }
     }
+    /**
+     * 退出登录
+     */
+    public function destroy(){
+        Auth::logout();
+        session()->flash('success','您已经成功退出！');
+        return redirect('login');
+    }
 }
