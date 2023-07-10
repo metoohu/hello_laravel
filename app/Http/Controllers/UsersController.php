49636ae7420f4e7a3c2d71296f8f87d44af4c0e7
edwin <metoohu@126.com>
2023-07-10 17:05:53 +0800

    登录成功创建会话

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,20 +7,31 @@ use App\Models\User;
 
 class UsersController extends Controller
 {
+    /**
+     * 用户注册页面
+     */
     public function create(){
         return view('users.create');
     }
-
+    /**
+     * 用户详情页面
+     */
     public function show(User $user){
         return view('users.show',compact('user'));
     }
-
+    /**
+     * 用户注册提交数据
+     */
     public function store(Request $request){
+        //用户注册字段验证
         $this->validate($request,[
             'name'=>'required|unique:users|max:50',
             'email'=>'required|email|unique:users|max:255',
             'password'=>'required|confirmed|min:6'
         ]);
+        /**
+         * 添加用户信息到用户表
+         */
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
