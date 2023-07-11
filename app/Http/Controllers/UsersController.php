04b9c3c428d4f418c0f4618d366075f1389cae89
edwin <metoohu@126.com>
2023-07-11 22:13:05 +0800

    查下用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -76,4 +76,11 @@ class UsersController extends Controller
         session()->flash('success','个人资料更新成功！');
         return redirect()->route('users.show',$user->id);
     }
+    /**
+     * 用户列表
+     */
+    public function index(){
+        $users = User::all();
+        return view('users.index',compact('users'));
+    }
 }
