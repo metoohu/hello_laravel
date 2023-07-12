27da8596c2ba7fc8ac3c2e6c51305d3ce25ee7fd
edwin <metoohu@126.com>
2023-07-12 17:42:41 +0800

    微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,7 +27,8 @@ class UsersController extends Controller
      * 用户详情页面
      */
     public function show(User $user){
-        return view('users.show',compact('user'));
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(30);
+        return view('users.show',compact('user','statuses'));
     }
     /**
      * 用户注册提交数据
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
         // ]);
         Model::unguard();
         $this->call(UsersTableseeder::class);
+        $this->call(StatusesTableSeeder::class);
         Model::reguard();
     }
 }
